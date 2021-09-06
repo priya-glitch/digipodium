799833928f4d5b725bebadc5bbabd637e5603bf7
Avneesh Yadav <avneeshyadav@Avneeshs-MacBook-Air.local>
2021-09-06 17:13:50 +0530

    msg

---
+++ section1/conditionsandloops.js
@@ -0,0 +1,46 @@
+age= 23;
+
+if(age>18){
+    console.log("Eligible");
+
+    x="abcd";
+    console.log(x);
+
+
+    let z ="let";
+    z="block";
+    console.log(z);
+
+
+    var a ="var";
+    a="functional";
+    console.log(a);
+
+
+    // const b="constant";
+    // console.log(b);
+    // b="block";
+    // console.log(b);
+
+    
+
+}
+else{
+    console.log("not Eligible");
+    
+}
+// console.log(z);
+//  console.log(b);
+console.log(a);
+
+
+
+for(let i=0; i<=10;i++){
+    console.log(i);
+}
+
+
+const iterable=[1,10];
+for(const x of iterable){
+console.log(x);
+}
---
+++ section1/index.js
@@ -0,0 +1,7 @@
+console.log("javascript on server");
+a =236;
+console.log(a);
+console.log(typeof(a));
+myname = "priya yadav";
+console.log(myname);
+
